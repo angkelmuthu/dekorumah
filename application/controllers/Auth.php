c900f5c932de4a4ee0a202416e9204c64ba1a0ad
angkelmuthu <angkel.muthu@gmail.com>
2023-01-17 12:34:01 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -25,7 +25,7 @@ class Auth extends CI_Controller
             if (password_verify($password, $user['password'])) {
                 // retrive user data to session
                 $this->session->set_userdata($user);
-                redirect('t_invoice');
+                redirect('M_pelanggan');
             } else {
                 redirect('auth');
             }
