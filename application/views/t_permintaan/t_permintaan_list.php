6f00ce8108c439d63b6b90e545a49f58afdce41e
angkelmuthu <angkel.muthu@gmail.com>
2023-01-17 12:25:59 +0700

    Update t_permintaan_list.php

--- application/views/t_permintaan/t_permintaan_list.php
+++ application/views/t_permintaan/t_permintaan_list.php
@@ -34,8 +34,8 @@
         </div>
     </div>
 </main>
-<script src="<?php echo base_url() ?>assets/smartadmin/js/vendors.bundle.js"></script>
-<script src="<?php echo base_url() ?>assets/smartadmin/js/app.bundle.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin-slim//js/vendors.bundle.js"></script>
+<script src="<?php echo base_url() ?>assets/smartadmin-slim//js/app.bundle.js"></script>
 <script src="<?php echo base_url() ?>assets/smartadmin/js/datagrid/datatables/datatables.bundle.js"></script>
 <script src="<?php echo base_url() ?>assets/smartadmin/js/datagrid/datatables/datatables.export.js"></script>
 <script type="text/javascript">
